01aeeb85073730ab6aadd3fdffa03e0d0938ef61
behulum <behulummd2018@gmail.com>
2020-10-04 20:11:45 -0400

    2nd update of #

---
+++ Pythagoream.java
@@ -0,0 +1,22 @@
+
+public class Pythagoream {
+  
+		public static void main(String[] args) {
+			Pythagoream p = new Pythagoream();
+			p.result();
+			
+
+		}
+		private void result() {
+			double x = 3;
+		     double y = 4;
+		     double result;
+		     x = Math.sqrt(x);
+		     y = Math.sqrt(y);
+		     result = x + y;
+		     System.out.println("The pythagoream is = " +result );
+		}
+
+	}
+
+}
